2bde3267cdeba07a48c88f1e3f4d85c194904772
Colleen Murphy <colleen@puppetlabs.com>
2014-06-17 16:48:04 -0700

    Add git repo management function

--- sync.rb
+++ sync.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'erb'
+require 'git'
 
 # git pull all repos
 # read config params
@@ -17,7 +18,6 @@ class ForgeModuleFile
     @rvms = rvms
   end
 end
-
 
 def build(from_erb_template)
   erb_obj = ERB.new(File.read(from_erb_template), nil, '-')
@@ -36,6 +36,16 @@ def sync(template, to_file)
   end
 end
 
+def update_repo(name, files, message)
+  repo = Git.open('../' + name)
+  repo.branch('master').checkout
+  repo.pull
+  repo.add(files)
+  repo.commit(message)
+  # TODO: repo.push
+end
+
 erb = build(MODULE_DIR + '.travis.yml')
 template = render(erb, { :rvms => ['1.8.7', '1.9.3', '2.0.0', '2.1.0'] })
 sync(template, '../' + modules[0] + '/' + '.travis.yml')
+update_repo(modules[0], ['.travis.yml'], 'testing updating .travis.yml')
