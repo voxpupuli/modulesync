b9b4480ede7a06d78e4f84c2c56ee439efcfa4ef
Joe Stump <joe.stump@salesforce.com>
2018-10-04 10:47:22 -0500

    Fix expect in tests.

--- spec/unit/modulesync_spec.rb
+++ spec/unit/modulesync_spec.rb
@@ -5,7 +5,7 @@ describe ModuleSync do
     it 'loads the managed modules from the specified :managed_modules_conf' do
       allow(ModuleSync).to receive(:local_files).and_return([])
       allow(ModuleSync::Util).to receive(:parse_config).with('./config_defaults.yml').and_return({})
-      expect(ModuleSync).to receive(:managed_modules).with('./test_file.yml', nil, nil).and_return([])
+      expect(ModuleSync).to receive(:managed_modules).with('./test_file.yml', nil, nil, nil).and_return([])
 
       options = { managed_modules_conf: 'test_file.yml' }
       ModuleSync.update(options)
