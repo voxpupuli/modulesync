069604e1fb425685e39dd53dcb495da6c3109951
Romuald Conty <romuald@opus-codium.fr>
2021-10-06 21:10:59 +0200

    Code coverage: Grab results when using Aruba
    To run behavior tests, `aruba` executes `bin/msync` directly. In order to allow simplecov to track files, etc. we need to setup it at application start.

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,4 +10,7 @@ ModuleSync::Faker.working_directory = File.expand_path('faker', Aruba.config.wor
 
 Before do
   @aruba_timeout_seconds = 5
+
+  # This enables coverage when aruba runs `msync` executable (cf. `bin/msync`)
+  set_environment_variable('COVERAGE', '1')
 end
