voxpupuli/modulesync

commit ee8623c53926b6cce7e6387438ad43901e609290

Drop EoL Ruby 2.5/2.6 support

lib/modulesync/renderer.rb
@@ -15,7 +15,7 @@ module ModuleSync
       erb_obj = if RUBY_VERSION >= '2.7'
                   ERB.new(template, trim_mode: '-')
                 else
-                  ERB.new(template, nil, '-')
+                  ERB.new(template, trim_mode: '-')
                 end
       erb_obj.filename = template_file
       erb_obj.def_method(ForgeModuleFile, 'render()', template_file)

.github/workflows/ci.yml
@@ -21,7 +21,7 @@ jobs:
           ruby-version: '3.1'
           bundler-cache: true
       - name: Run linter
-        run: bundle exec rubocop --format github
+        run: bundle exec rake rubocop
   test:
     needs: rubocop
     runs-on: ubuntu-latest
@@ -29,8 +29,6 @@ jobs:
       fail-fast: false
       matrix:
         include:
-          - ruby: "2.5"
-          - ruby: "2.6"
           - ruby: "2.7"
           - ruby: "3.0"
           - ruby: "3.1"
@@ -50,7 +48,7 @@ jobs:
       - name: Run behavior tests
         run: bundle exec cucumber
       - name: Build gem
-        run: gem build *.gemspec
+        run: gem build --verbose *.gemspec
   tests:
     needs:
       - rubocop

.github/workflows/release.yml
@@ -18,7 +18,7 @@ jobs:
         env:
           BUNDLE_WITHOUT: release
       - name: Build gem
-        run: gem build *.gemspec
+        run: gem build --verbose *.gemspec
       - name: Publish gem to rubygems.org
         run: gem push *.gem
         env:

.rubocop.yml
@@ -1,50 +1,5 @@
+---
 inherit_from: .rubocop_todo.yml
 
-require:
-  - rubocop-performance
-  - rubocop-rake
-  - rubocop-rspec
-
-AllCops:
-  TargetRubyVersion: 2.5
-  NewCops: enable
-  Exclude:
-    - 'vendor/**/*'
-    - 'tmp/**/*'
-    - 'pkg/**/*'
-    - 'lib/monkey_patches.rb'
-    - 'spec/**/*'
-    - 'Gemfile'
-    - 'Rakefile'
-
-Style/FetchEnvVar:
-  Enabled: false
-
-Style/HashSyntax:
-  Enabled: false
-
-Style/TrailingCommaInArguments:
-  EnforcedStyleForMultiline: comma
-
-Style/TrailingCommaInArrayLiteral:
-  EnforcedStyleForMultiline: comma
-
-Style/TrailingCommaInHashLiteral:
-  EnforcedStyleForMultiline: comma
-
-# sane line length
-Layout/LineLength:
-  Max: 120
-  Exclude:
-    - 'features/**/*'
-
-Style/FrozenStringLiteralComment:
-  Enabled: false
-
-# We explicitly prefer $stderr.puts over #warn
-Style/StderrPuts:
-  Enabled: false
-
-Gemspec/RequireMFA:
-  Exclude:
-    - 'modulesync.gemspec'
+inherit_gem:
+  voxpupuli-rubocop: rubocop.yml

.rubocop_todo.yml
@@ -1,37 +1,114 @@
 # This configuration was generated by
 # `rubocop --auto-gen-config`
-# on 2023-04-14 18:50:10 UTC using RuboCop version 1.28.2.
+# on 2023-06-16 18:17:35 UTC using RuboCop version 1.50.2.
 # The point is for the user to remove these configuration records
 # one by one as the offenses are removed from the code base.
 # Note that changes in the inspected code, or installation of new
 # versions of RuboCop, may require this file to be generated again.
 
-# Offense count: 10
-# Configuration parameters: IgnoredMethods, CountRepeatedAttributes.
-Metrics/AbcSize:
-  Max: 60
+# Offense count: 1
+# Configuration parameters: IgnoreLiteralBranches, IgnoreConstantBranches.
+Lint/DuplicateBranch:
+  Exclude:
+    - 'lib/modulesync/renderer.rb'
 
-# Offense count: 2
-# Configuration parameters: CountComments, CountAsOne.
-Metrics/ClassLength:
-  Max: 186
+# Offense count: 1
+# Configuration parameters: AllowComments, AllowNil.
+Lint/SuppressedException:
+  Exclude:
+    - 'Rakefile'
+
+# Offense count: 1
+# This cop supports unsafe autocorrection (--autocorrect-all).
+Performance/StringInclude:
+  Exclude:
+    - 'Rakefile'
 
 # Offense count: 5
-# Configuration parameters: IgnoredMethods.
-Metrics/CyclomaticComplexity:
-  Max: 15
+# This cop supports unsafe autocorrection (--autocorrect-all).
+RSpec/BeEq:
+  Exclude:
+    - 'spec/unit/modulesync/settings_spec.rb'
+
+# Offense count: 3
+# Configuration parameters: Prefixes, AllowedPatterns.
+# Prefixes: when, with, without
+RSpec/ContextWording:
+  Exclude:
+    - 'spec/unit/modulesync/git_service/github_spec.rb'
+    - 'spec/unit/modulesync/git_service/gitlab_spec.rb'
+    - 'spec/unit/modulesync_spec.rb'
 
 # Offense count: 17
-# Configuration parameters: CountComments, CountAsOne, ExcludedMethods, IgnoredMethods.
-Metrics/MethodLength:
-  Max: 34
+# This cop supports unsafe autocorrection (--autocorrect-all).
+# Configuration parameters: SkipBlocks, EnforcedStyle.
+# SupportedStyles: described_class, explicit
+RSpec/DescribedClass:
+  Exclude:
+    - 'spec/unit/modulesync/git_service/factory_spec.rb'
+    - 'spec/unit/modulesync/git_service/github_spec.rb'
+    - 'spec/unit/modulesync/git_service/gitlab_spec.rb'
+    - 'spec/unit/modulesync/git_service_spec.rb'
+    - 'spec/unit/modulesync/settings_spec.rb'
+    - 'spec/unit/modulesync/source_code_spec.rb'
+    - 'spec/unit/modulesync_spec.rb'
 
-# Offense count: 4
-# Configuration parameters: IgnoredMethods.
-Metrics/PerceivedComplexity:
+# Offense count: 11
+# Configuration parameters: CountAsOne.
+RSpec/ExampleLength:
   Max: 16
 
-# Offense count: 8
+# Offense count: 7
+# Configuration parameters: Include, CustomTransform, IgnoreMethods, SpecSuffixOnly.
+# Include: **/*_spec*rb*, **/spec/**/*
+RSpec/FilePath:
+  Exclude:
+    - 'spec/unit/modulesync/git_service/factory_spec.rb'
+    - 'spec/unit/modulesync/git_service/github_spec.rb'
+    - 'spec/unit/modulesync/git_service/gitlab_spec.rb'
+    - 'spec/unit/modulesync/git_service_spec.rb'
+    - 'spec/unit/modulesync/settings_spec.rb'
+    - 'spec/unit/modulesync/source_code_spec.rb'
+    - 'spec/unit/modulesync_spec.rb'
+
+# Offense count: 19
+# Configuration parameters: AssignmentOnly.
+RSpec/InstanceVariable:
+  Exclude:
+    - 'spec/unit/modulesync/git_service/github_spec.rb'
+    - 'spec/unit/modulesync/git_service/gitlab_spec.rb'
+
+# Offense count: 7
+# Configuration parameters: .
+# SupportedStyles: have_received, receive
+RSpec/MessageSpies:
+  EnforcedStyle: receive
+
+# Offense count: 6
+RSpec/MultipleExpectations:
+  Max: 2
+
+# Offense count: 10
+# Configuration parameters: EnforcedStyle, IgnoreSharedExamples.
+# SupportedStyles: always, named_only
+RSpec/NamedSubject:
+  Exclude:
+    - 'spec/unit/modulesync/settings_spec.rb'
+    - 'spec/unit/modulesync/source_code_spec.rb'
+
+# Offense count: 9
+# Configuration parameters: AllowedGroups.
+RSpec/NestedGroups:
+  Max: 5
+
+# Offense count: 6
+RSpec/StubbedMock:
+  Exclude:
+    - 'spec/unit/modulesync/git_service/github_spec.rb'
+    - 'spec/unit/modulesync/git_service/gitlab_spec.rb'
+    - 'spec/unit/modulesync_spec.rb'
+
+# Offense count: 9
 # Configuration parameters: AllowedConstants.
 Style/Documentation:
   Exclude:
@@ -42,6 +119,27 @@ Style/Documentation:
     - 'lib/modulesync/hook.rb'
     - 'lib/modulesync/renderer.rb'
     - 'lib/modulesync/util.rb'
+    - 'lib/monkey_patches.rb'
+
+# Offense count: 4
+# This cop supports safe autocorrection (--autocorrect).
+# Configuration parameters: AllowedVars.
+Style/FetchEnvVar:
+  Exclude:
+    - 'lib/modulesync/git_service.rb'
+
+# Offense count: 34
+# This cop supports unsafe autocorrection (--autocorrect-all).
+# Configuration parameters: EnforcedStyle.
+# SupportedStyles: always, always_true, never
+Style/FrozenStringLiteralComment:
+  Enabled: false
+
+# Offense count: 2
+# This cop supports unsafe autocorrection (--autocorrect-all).
+Style/IdenticalConditionalBranches:
+  Exclude:
+    - 'lib/modulesync/renderer.rb'
 
 # Offense count: 1
 # Configuration parameters: AllowedMethods.
@@ -49,3 +147,10 @@ Style/Documentation:
 Style/OptionalBooleanParameter:
   Exclude:
     - 'lib/modulesync/puppet_module.rb'
+
+# Offense count: 6
+# This cop supports safe autocorrection (--autocorrect).
+# Configuration parameters: AllowHeredoc, AllowURI, URISchemes, IgnoreCopDirectives, AllowedPatterns.
+# URISchemes: http, https
+Layout/LineLength:
+  Max: 169

Gemfile
@@ -3,10 +3,10 @@ source ENV['GEM_SOURCE'] || 'https://rubygems.org'
 gemspec
 
 group :release do
-  gem 'github_changelog_generator', :require => false
+  gem 'github_changelog_generator', require: false
 end
 
-group :coverage, optional: ENV['CODECOV']!='yes' do
-  gem 'simplecov-console', :require => false
-  gem 'codecov', :require => false
+group :coverage, optional: ENV['CODECOV'] != 'yes' do
+  gem 'codecov', require: false
+  gem 'simplecov-console', require: false
 end

Rakefile
@@ -10,16 +10,9 @@ rescue LoadError
 end
 
 begin
-  require 'rubocop/rake_task'
+  require 'voxpupuli/rubocop/rake'
 rescue LoadError
   # RuboCop is an optional group
-else
-  RuboCop::RakeTask.new(:rubocop) do |task|
-    # These make the rubocop experience maybe slightly less terrible
-    task.options = ['--display-cop-names', '--display-style-guide', '--extra-details']
-    # Use Rubocop's Github Actions formatter if possible
-    task.formatters << 'github' if ENV['GITHUB_ACTIONS'] == 'true'
-  end
 end
 
 CLEAN.include('pkg/', 'tmp/')
@@ -29,8 +22,8 @@ Cucumber::Rake::Task.new do |t|
   t.cucumber_opts << '--format pretty'
 end
 
-task :test => %i[clean spec cucumber rubocop]
-task :default => %i[test]
+task test: %i[clean spec cucumber rubocop]
+task default: %i[test]
 
 begin
   require 'github_changelog_generator/task'
@@ -44,7 +37,7 @@ begin
 
   # Workaround for https://github.com/github-changelog-generator/github-changelog-generator/issues/715
   require 'rbconfig'
-  if RbConfig::CONFIG['host_os'] =~ /linux/
+  if /linux/.match?(RbConfig::CONFIG['host_os'])
     task :changelog do
       puts 'Fixing line endings...'
       changelog_file = File.join(__dir__, 'CHANGELOG.md')

lib/modulesync.rb
@@ -12,17 +12,17 @@ require 'modulesync/util'
 
 require 'monkey_patches'
 
-module ModuleSync # rubocop:disable Metrics/ModuleLength
+module ModuleSync
   class Error < StandardError; end
 
   include Constants
 
   def self.config_defaults
     {
-      :project_root => 'modules/',
-      :managed_modules_conf => 'managed_modules.yml',
-      :configs => '.',
-      :tag_pattern => '%s',
+      project_root: 'modules/',
+      managed_modules_conf: 'managed_modules.yml',
+      configs: '.',
+      tag_pattern: '%s',
     }
   end
 
@@ -33,7 +33,7 @@ module ModuleSync # rubocop:disable Metrics/ModuleLength
   def self.local_file(config_path, file)
     path = File.join(config_path, MODULE_FILES_DIR, file)
     if !File.exist?("#{path}.erb") && File.exist?(path)
-      $stderr.puts "Warning: using '#{path}' as template without '.erb' suffix"
+      warn "Warning: using '#{path}' as template without '.erb' suffix"
       path
     else
       "#{path}.erb"
@@ -50,9 +50,9 @@ module ModuleSync # rubocop:disable Metrics/ModuleLength
           .collect { |p| p.chomp('.erb') }
           .to_a
     else
-      $stderr.puts "#{local_template_dir} does not exist." \
-                   ' Check that you are working in your module configs directory or' \
-                   ' that you have passed in the correct directory with -c.'
+      warn "#{local_template_dir} does not exist. " \
+           'Check that you are working in your module configs directory or ' \
+           'that you have passed in the correct directory with -c.'
       exit 1
     end
   end
@@ -68,8 +68,8 @@ module ModuleSync # rubocop:disable Metrics/ModuleLength
 
     managed_modules = Util.parse_config(config_file)
     if managed_modules.empty?
-      $stderr.puts "No modules found in #{config_file}." \
-                   ' Check that you specified the right :configs directory and :managed_modules_conf file.'
+      warn "No modules found in #{config_file}. " \
+           'Check that you specified the right :configs directory and :managed_modules_conf file.'
       exit 1
     end
     managed_modules.select! { |m| m =~ Regexp.new(filter) } unless filter.nil?
@@ -99,16 +99,16 @@ module ModuleSync # rubocop:disable Metrics/ModuleLength
         erb = Renderer.build(template_file)
         # Meta data passed to the template as @metadata[:name]
         metadata = {
-          :module_name => settings.additional_settings[:puppet_module],
-          :namespace => settings.additional_settings[:namespace],
-          :workdir => puppet_module.working_directory,
-          :target_file => target_file,
+          module_name: settings.additional_settings[:puppet_module],
+          namespace: settings.additional_settings[:namespace],
+          workdir: puppet_module.working_directory,
+          target_file: target_file,
         }
         template = Renderer.render(erb, configs, metadata)
         mode = File.stat(template_file).mode
         Renderer.sync(template, target_file, mode)
       rescue StandardError
-        $stderr.puts "#{puppet_module.given_name}: Error while rendering file: '#{filename}'"
+        warn "#{puppet_module.given_name}: Error while rendering file: '#{filename}'"
         raise
       end
     end
@@ -128,9 +128,9 @@ module ModuleSync # rubocop:disable Metrics/ModuleLength
                             defaults,
                             module_configs[GLOBAL_DEFAULTS_KEY] || {},
                             module_configs,
-                            :puppet_module => puppet_module.repository_name,
-                            :git_base => options[:git_base],
-                            :namespace => puppet_module.repository_namespace)
+                            puppet_module: puppet_module.repository_name,
+                            git_base: options[:git_base],
+                            namespace: puppet_module.repository_namespace)
 
     settings.unmanaged_files(module_files).each do |filename|
       $stdout.puts "Not managing '#{filename}' in '#{puppet_module.given_name}'"
@@ -178,7 +178,7 @@ module ModuleSync # rubocop:disable Metrics/ModuleLength
       manage_module(puppet_module, module_files, defaults)
     rescue ModuleSync::Error, Git::GitExecuteError => e
       message = e.message || 'Error during `update`'
-      $stderr.puts "#{puppet_module.given_name}: #{message}"
+      warn "#{puppet_module.given_name}: #{message}"
       exit 1 unless options[:skip_broken]
       errors = true
       $stdout.puts "Skipping '#{puppet_module.given_name}' as update process failed"
@@ -222,7 +222,7 @@ module ModuleSync # rubocop:disable Metrics/ModuleLength
         raise Thor::Error, message if @options[:fail_fast]
 
         errors[puppet_module.given_name] = message
-        $stderr.puts message
+        warn message
       end
 
       $stdout.puts ''

commit c28a49132b97e273d94211d0822e9455d670e7b4

Clean up redundant statement
In ee8623c53926b6cce7e6387438ad43901e609290 the support for old versions
was dropped, but this resulted in an if statement with the same code in
both branches.

Fixes: ee8623c53926 ("Drop EoL Ruby 2.5/2.6 support")

lib/modulesync/renderer.rb
@@ -12,11 +12,7 @@ module ModuleSync
 
     def self.build(template_file)
       template = File.read(template_file)
-      erb_obj = if RUBY_VERSION >= '2.7'
-                  ERB.new(template, trim_mode: '-')
-                else
-                  ERB.new(template, trim_mode: '-')
-                end
+      erb_obj = ERB.new(template, trim_mode: '-')
       erb_obj.filename = template_file
       erb_obj.def_method(ForgeModuleFile, 'render()', template_file)
       erb_obj